5beb7741ac02d9754b0919602ec71ec1ec0c8d5f
Adam J Lemmon <adamjlemmon@gmail.com>
2020-05-06 06:57:51 -0400

    test: 💍 Add missing signedProof test for doc without proof

--- src/verifiers/signedProof/openAttestationSignedProof.test.ts
+++ src/verifiers/signedProof/openAttestationSignedProof.test.ts
@@ -3,9 +3,30 @@ import { openAttestationSignedProof } from "./openAttestationSignedProof";
 import { documentSignedProofValid } from "../../../test/fixtures/v2/documentSignedProofValid";
 import { documentSignedProofInvalidProofType } from "../../../test/fixtures/v2/documentSignedProofInvalidProofType";
 import { documentSignedProofInvalidSignature } from "../../../test/fixtures/v2/documentSignedProofInvalidSignature";
+import { documentMainnetValidWithCertificateStore } from "../../../test/fixtures/v2/documentMainnetValidWithCertificateStore";
+
 const verify = verificationBuilder([openAttestationSignedProof]);
 
 describe("OpenAttestationSignedProof", () => {
+  describe("SKIPPED", () => {
+    it("should return a valid SKIPPED fragment when document does not have a proof a block", async () => {
+      const fragment = await verify(documentMainnetValidWithCertificateStore, {
+        network: "ropsten"
+      });
+      expect(fragment).toStrictEqual([
+        {
+          name: "OpenAttestationSignedProof",
+          type: "DOCUMENT_STATUS",
+          reason: {
+            code: 4,
+            codeString: "SKIPPED",
+            message: "Document does not have a proof block"
+          },
+          status: "SKIPPED"
+        }
+      ]);
+    });
+  });
   describe("v2", () => {
     it("should return a valid fragment when document has valid signed proof", async () => {
       const fragment = await verify(documentSignedProofValid, {
