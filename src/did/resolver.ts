a26a0718d54b2e2653a41485018b8b014f5f8c26
Raymond Yeh <ray@geek.sg>
2020-09-18 11:20:22 +0800

    feat: node cache for did resolution

--- src/did/resolver.ts
+++ src/did/resolver.ts
@@ -1,24 +1,26 @@
 import { Resolver, DIDDocument } from "did-resolver";
 import { getResolver as ethrGetResolver } from "ethr-did-resolver";
 import { getResolver as webGetResolver } from "web-did-resolver";
+import NodeCache from "node-cache";
 import { INFURA_API_KEY } from "../config";
 
 const providerConfig = {
   networks: [{ name: "mainnet", rpcUrl: `https://mainnet.infura.io/v3/${INFURA_API_KEY}` }],
 };
 
+const didResolutionCache = new NodeCache({ stdTTL: 5 * 60 }); // 5 min
+
 export const resolver = new Resolver({
   ...ethrGetResolver(providerConfig),
   ...webGetResolver(),
 });
 
-// TODO replace cache with timeout
-const cache: { [didUrl: string]: DIDDocument } = {};
-
 export const resolve = async (didUrl: string): Promise<DIDDocument> => {
-  if (cache[didUrl]) return cache[didUrl];
-  cache[didUrl] = await resolver.resolve(didUrl);
-  return cache[didUrl];
+  const cachedResult = didResolutionCache.get<DIDDocument>(didUrl);
+  if (cachedResult) return cachedResult;
+  const did = await resolver.resolve(didUrl);
+  didResolutionCache.set(didUrl, did);
+  return did;
 };
 
 export const getPublicKey = async (did: string, key: string) => {
