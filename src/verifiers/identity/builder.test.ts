4d695b64c4a521a251e22124eeeaf42af4bb8772
Raymond Yeh <ray@geek.sg>
2020-10-21 14:35:40 +0800

    feat: identity verification builder

---
+++ src/verifiers/identity/builder.test.ts
@@ -0,0 +1,42 @@
+import { issuerIdentityVerifierBuilder, IssuerIdentityVerifier, IssuerIdentityVerifierDefinition } from "./verifier";
+
+const mockVerify: IssuerIdentityVerifier = () => {
+  return Promise.resolve({
+    verifier: "MOCK_VERIFIER",
+    identifier: "DID:METHOD:IDENTIFIER",
+    status: "VALID",
+  });
+};
+
+const mockVerifyDef: IssuerIdentityVerifierDefinition = {
+  type: "MOCK_TYPE",
+  verify: mockVerify,
+};
+
+describe("issuerIdentityVerifierBuilder", () => {
+  it("should build a verifier with the right functions", () => {
+    const verifier = issuerIdentityVerifierBuilder([mockVerifyDef]);
+    expect(verifier.test).toBeTruthy();
+    expect(verifier.skip).toBeTruthy();
+    expect(verifier.verify).toBeTruthy();
+  });
+  it("should run for all documents", () => {
+    const verifier = issuerIdentityVerifierBuilder([mockVerifyDef]);
+    expect(verifier.test({} as any, {} as any)).toBe(true);
+  });
+  it("should return the correct skip message", async () => {
+    const verifier = issuerIdentityVerifierBuilder([mockVerifyDef]);
+    expect(await verifier.skip({} as any, {} as any)).toMatchInlineSnapshot(`
+      Object {
+        "name": "OpenAttestationIssuerIdentityVerifier",
+        "reason": Object {
+          "code": 0,
+          "codeString": "SKIPPED",
+          "message": "Verification of issuers' identity skipped",
+        },
+        "status": "SKIPPED",
+        "type": "ISSUER_IDENTITY",
+      }
+    `);
+  });
+});
---
+++ src/verifiers/identity/builder.ts
@@ -0,0 +1,133 @@
+import { v2, v3, WrappedDocument, utils, getData } from "@govtechsg/open-attestation";
+import {
+  VerificationFragmentType,
+  Verifier,
+  VerificationFragmentStatus,
+  SkippedVerificationFragment,
+} from "../../types/core";
+import { Reason } from "../../types/error";
+import { OpenAttestationIssuerIdentityVerifierCode } from "../../types/error";
+
+const name = "OpenAttestationIssuerIdentityVerifier";
+const type: VerificationFragmentType = "ISSUER_IDENTITY";
+type VerifierType = Verifier<WrappedDocument<v2.OpenAttestationDocument> | WrappedDocument<v3.OpenAttestationDocument>>;
+
+export interface VerifierResults<T = any> {
+  verifier: string; // Which verifier returned this result
+  identifier?: string; // Human readable interpretation of the issuer
+  status: VerificationFragmentStatus; // Status of this verification result
+  data?: T; // Other metadata returned by verifier
+  reason?: Reason; // Reasons for status
+}
+
+export type IssuerIdentityVerifier<
+  Document = WrappedDocument<v3.OpenAttestationDocument> | WrappedDocument<v2.OpenAttestationDocument>,
+  ResultData = any
+> = (document: Document, issuerIndex?: number) => Promise<VerifierResults<ResultData>>;
+
+export type IssuerIdentityVerifierDefinition = {
+  type: string;
+  verify: IssuerIdentityVerifier;
+};
+
+const skipFragment: SkippedVerificationFragment = {
+  status: "SKIPPED",
+  type,
+  name,
+  reason: {
+    code: OpenAttestationIssuerIdentityVerifierCode.SKIPPED,
+    codeString: OpenAttestationIssuerIdentityVerifierCode[OpenAttestationIssuerIdentityVerifierCode.SKIPPED],
+    message: `Verification of issuers' identity skipped`,
+  },
+};
+
+const defaultVerifier: IssuerIdentityVerifier = async (_, index) => {
+  return {
+    verifier: "SKIPPED_VERIFIER",
+    status: "SKIPPED",
+    reason: {
+      code: OpenAttestationIssuerIdentityVerifierCode.SKIPPED,
+      codeString: OpenAttestationIssuerIdentityVerifierCode[OpenAttestationIssuerIdentityVerifierCode.SKIPPED],
+      message: `No verifier found for issuer ${index}`,
+    },
+  };
+};
+
+export const issuerIdentityVerifierBuilder = (verifiers: IssuerIdentityVerifierDefinition[]): VerifierType => {
+  const registeredVerifiers: { [identityProofType: string]: IssuerIdentityVerifier } = {};
+  verifiers.forEach((verifier) => (registeredVerifiers[verifier.type] = verifier.verify));
+
+  const getVerifier = (identityProofType: string): IssuerIdentityVerifier =>
+    registeredVerifiers[identityProofType] || defaultVerifier;
+  const skip: VerifierType["skip"] = async () => skipFragment;
+  const test: VerifierType["test"] = () => true;
+
+  const verify: VerifierType["verify"] = async (document) => {
+    try {
+      if (utils.isWrappedV3Document(document)) {
+        const documentData = getData(document);
+        const verifier = getVerifier(documentData.issuer.identityProof.type);
+        const verificationResults = await verifier(document);
+        return {
+          name,
+          type,
+          data: verificationResults,
+          status: verificationResults.status,
+        };
+      }
+      if (utils.isWrappedV2Document(document)) {
+        const documentData = getData(document);
+        const verificationResultsDeferred = documentData.issuers.map((issuer, index) => {
+          const identityProofType = issuer.identityProof?.type;
+          if (!identityProofType) throw new Error("");
+          const verifier = getVerifier(identityProofType);
+          return verifier(document, index);
+        });
+        const verificationResults = await Promise.all(verificationResultsDeferred);
+        const overallStatus =
+          verificationResults.every((result) => result.status === "VALID") && documentData.issuers.length > 0
+            ? "VALID"
+            : "INVALID";
+        return {
+          name,
+          type,
+          data: verificationResults,
+          status: overallStatus,
+        };
+      }
+      return {
+        name,
+        type,
+        status: "INVALID",
+        reason: {
+          code: OpenAttestationIssuerIdentityVerifierCode.UNEXPECTED_DOCUMENT_FORMAT,
+          codeString:
+            OpenAttestationIssuerIdentityVerifierCode[
+              OpenAttestationIssuerIdentityVerifierCode.UNEXPECTED_DOCUMENT_FORMAT
+            ],
+          message: `Document format is unexpected`,
+        },
+      };
+    } catch (e) {
+      return {
+        name,
+        type,
+        data: e,
+        reason: {
+          message: e.message,
+          code: e.code || OpenAttestationIssuerIdentityVerifierCode.UNEXPECTED_ERROR,
+          codeString:
+            e.codeString ||
+            OpenAttestationIssuerIdentityVerifierCode[OpenAttestationIssuerIdentityVerifierCode.UNEXPECTED_ERROR],
+        },
+        status: "ERROR",
+      };
+    }
+  };
+
+  return {
+    skip,
+    test,
+    verify,
+  };
+};
