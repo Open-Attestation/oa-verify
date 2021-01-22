806cc34bee6e1361b6c50d6e6ccd885655763e25
Raymond Yeh <ray@geek.sg>
2021-01-22 11:50:09 +0800

    feat: documentIntegrity/hash working for v3

--- src/verifiers/issuerIdentity/dnsTxt/openAttestationDnsTxt.v3.test.ts
+++ src/verifiers/issuerIdentity/dnsTxt/openAttestationDnsTxt.v3.test.ts
@@ -1,11 +1,11 @@
 describe("OpenAttestationDnsTxt v3 document", () => {
-  xit("should return a valid fragment when document has valid identity", async () => {});
-  xit("should return an invalid fragment when document identity does not match", async () => {});
-  xit("should return an error fragment when document has no identity type", async () => {});
-  xit("should return an error fragment when document has no identity location", async () => {});
+  it.skip("should return a valid fragment when document has valid identity", async () => {});
+  it.skip("should return an invalid fragment when document identity does not match", async () => {});
+  it.skip("should return an error fragment when document has no identity type", async () => {});
+  it.skip("should return an error fragment when document has no identity location", async () => {});
 
   describe("test", () => {
-    xit("should return true if identityProof type is DNS-TXT", () => {});
-    xit("should return false if identityProof type is not DNS-TXT", () => {});
+    it.skip("should return true if identityProof type is DNS-TXT", () => {});
+    it.skip("should return false if identityProof type is not DNS-TXT", () => {});
   });
 });
