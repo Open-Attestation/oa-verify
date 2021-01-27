9501f11c6e95c61162d7784609ab2e451b9a786b
Raymond Yeh <ray@geek.sg>
2021-01-27 15:13:44 +0800

    feat: verification for dns-did

--- src/verifiers/issuerIdentity/dnsDid/dnsDidProof.test.ts
+++ src/verifiers/issuerIdentity/dnsDid/dnsDidProof.test.ts
@@ -1,3 +1,4 @@
+import { v3 } from "@govtechsg/open-attestation";
 import { openAttestationDnsDidIdentityProof } from "./dnsDidProof";
 import { documentRopstenValidWithDocumentStore } from "../../../../test/fixtures/v2/documentRopstenValidWithDocumentStore";
 import { documentDidSigned } from "../../../../test/fixtures/v2/documentDidSigned";
@@ -8,6 +9,18 @@ import {
   documentDnsDidMixedTokenRegistryInvalid,
 } from "../../../../test/fixtures/v2/documentDnsDidMixedTokenRegistry";
 import { getProvider } from "../../../common/utils";
+
+import v3DnsDidWrappedRaw from "../../../../test/fixtures/v3/dnsdid-wrapped.json";
+import v3DidSignedRaw from "../../../../test/fixtures/v3/did-signed.json";
+import v3DnsDidSignedRaw from "../../../../test/fixtures/v3/dnsdid-signed.json";
+import v3DocumentStoreIssuedRaw from "../../../../test/fixtures/v3/documentStore-issued.json";
+import v3TokenRegistryIssuedRaw from "../../../../test/fixtures/v3/tokenRegistry-issued.json";
+
+const v3DidSigned = v3DidSignedRaw as v3.SignedWrappedDocument;
+const v3DnsDidWrapped = v3DnsDidWrappedRaw as v3.WrappedDocument;
+const v3DnsDidSigned = v3DnsDidSignedRaw as v3.SignedWrappedDocument;
+const v3DocumentStoreIssued = v3DocumentStoreIssuedRaw as v3.WrappedDocument;
+const v3TokenRegistryIssued = v3TokenRegistryIssuedRaw as v3.WrappedDocument;
 
 const options = {
   provider: getProvider({ network: "ropsten" }),
@@ -43,98 +56,153 @@ describe("test", () => {
       expect(openAttestationDnsDidIdentityProof.test(documentDnsDidSigned, options)).toBe(true);
     });
   });
+  describe("v3", () => {
+    it("should return true for document using `DNS-DID` top level identity proof", () => {
+      expect(openAttestationDnsDidIdentityProof.test(v3DnsDidSigned, options)).toBe(true);
+      expect(openAttestationDnsDidIdentityProof.test(v3DnsDidWrapped, options)).toBe(true);
+    });
+    it("should return false for document using `DID` top level identity proof", () => {
+      expect(openAttestationDnsDidIdentityProof.test(v3DidSigned, options)).toBe(false);
+    });
+    it("should return false for document using `DNS-TXT` top level identity proof", () => {
+      expect(openAttestationDnsDidIdentityProof.test(v3DocumentStoreIssued, options)).toBe(false);
+      expect(openAttestationDnsDidIdentityProof.test(v3TokenRegistryIssued, options)).toBe(false);
+    });
+  });
 });
 
 describe("verify", () => {
-  it("should verify a document with dns binding to did", async () => {
-    const fragment = await openAttestationDnsDidIdentityProof.verify(documentDnsDidSigned, options);
-    expect(fragment).toMatchInlineSnapshot(`
-      Object {
-        "data": Array [
-          Object {
+  describe("v2", () => {
+    it("should verify a document with dns binding to did", async () => {
+      const fragment = await openAttestationDnsDidIdentityProof.verify(documentDnsDidSigned, options);
+      expect(fragment).toMatchInlineSnapshot(`
+        Object {
+          "data": Array [
+            Object {
+              "key": "did:ethr:0xE712878f6E8d5d4F9e87E10DA604F9cB564C9a89#controller",
+              "location": "example.tradetrust.io",
+              "status": "VALID",
+            },
+          ],
+          "name": "OpenAttestationDnsDidIdentityProof",
+          "status": "VALID",
+          "type": "ISSUER_IDENTITY",
+        }
+      `);
+    });
+    it("should verify a document without dns binding to did", async () => {
+      const fragment = await openAttestationDnsDidIdentityProof.verify(documentDnsDidNoDnsTxt, options);
+      expect(fragment).toMatchInlineSnapshot(`
+        Object {
+          "data": Array [
+            Object {
+              "key": "did:ethr:0xE712878f6E8d5d4F9e87E10DA604F9cB564C9a89#controller",
+              "location": "example.com",
+              "status": "INVALID",
+            },
+          ],
+          "name": "OpenAttestationDnsDidIdentityProof",
+          "status": "INVALID",
+          "type": "ISSUER_IDENTITY",
+        }
+      `);
+    });
+    it("should fail if document has issuers not using DNS-DID", async () => {
+      const validFragment = await openAttestationDnsDidIdentityProof.verify(
+        documentDnsDidMixedTokenRegistryValid,
+        options
+      );
+      expect(validFragment).toMatchInlineSnapshot(`
+        Object {
+          "data": Array [
+            Object {
+              "key": "did:ethr:0xE712878f6E8d5d4F9e87E10DA604F9cB564C9a89#controller",
+              "location": "example.tradetrust.io",
+              "status": "VALID",
+            },
+            Object {
+              "reason": Object {
+                "code": 3,
+                "codeString": "INVALID_ISSUERS",
+                "message": "Issuer is not using DID-DNS identityProof type",
+              },
+              "status": "INVALID",
+            },
+          ],
+          "name": "OpenAttestationDnsDidIdentityProof",
+          "status": "INVALID",
+          "type": "ISSUER_IDENTITY",
+        }
+      `);
+
+      const invalidFragment = await openAttestationDnsDidIdentityProof.verify(
+        documentDnsDidMixedTokenRegistryInvalid,
+        options
+      );
+      expect(invalidFragment).toMatchInlineSnapshot(`
+        Object {
+          "data": Array [
+            Object {
+              "key": "did:ethr:0xE712878f6E8d5d4F9e87E10DA604F9cB564C9a89#controller",
+              "location": "example.com",
+              "status": "INVALID",
+            },
+            Object {
+              "reason": Object {
+                "code": 3,
+                "codeString": "INVALID_ISSUERS",
+                "message": "Issuer is not using DID-DNS identityProof type",
+              },
+              "status": "INVALID",
+            },
+          ],
+          "name": "OpenAttestationDnsDidIdentityProof",
+          "status": "INVALID",
+          "type": "ISSUER_IDENTITY",
+        }
+      `);
+    });
+  });
+  describe("v3", () => {
+    it("should return valid fragment for document with dns binding to did", async () => {
+      const fragment = await openAttestationDnsDidIdentityProof.verify(v3DnsDidSigned, options);
+      expect(fragment).toMatchInlineSnapshot(`
+        Object {
+          "data": Object {
             "key": "did:ethr:0xE712878f6E8d5d4F9e87E10DA604F9cB564C9a89#controller",
             "location": "example.tradetrust.io",
             "status": "VALID",
           },
-        ],
-        "name": "OpenAttestationDnsDidIdentityProof",
-        "status": "VALID",
-        "type": "ISSUER_IDENTITY",
-      }
-    `);
-  });
-
-  it("should verify a document without dns binding to did", async () => {
-    const fragment = await openAttestationDnsDidIdentityProof.verify(documentDnsDidNoDnsTxt, options);
-    expect(fragment).toMatchInlineSnapshot(`
-      Object {
-        "data": Array [
-          Object {
+          "name": "OpenAttestationDnsDidIdentityProof",
+          "status": "VALID",
+          "type": "ISSUER_IDENTITY",
+        }
+      `);
+    });
+    it("should return invalid fragment for document without dns binding to did", async () => {
+      const documentWithoutDnsBinding = {
+        ...v3DnsDidSigned,
+        openAttestationMetadata: {
+          ...v3DnsDidSigned.openAttestationMetadata,
+          identityProof: {
+            type: v3.IdentityProofType.DNSDid,
+            identifier: "example.com",
+          },
+        },
+      };
+      const fragment = await openAttestationDnsDidIdentityProof.verify(documentWithoutDnsBinding, options);
+      expect(fragment).toMatchInlineSnapshot(`
+        Object {
+          "data": Object {
             "key": "did:ethr:0xE712878f6E8d5d4F9e87E10DA604F9cB564C9a89#controller",
             "location": "example.com",
             "status": "INVALID",
           },
-        ],
-        "name": "OpenAttestationDnsDidIdentityProof",
-        "status": "INVALID",
-        "type": "ISSUER_IDENTITY",
-      }
-    `);
-  });
-
-  it("should fail if document has issuers not using DNS-DID", async () => {
-    const validFragment = await openAttestationDnsDidIdentityProof.verify(
-      documentDnsDidMixedTokenRegistryValid,
-      options
-    );
-    expect(validFragment).toMatchInlineSnapshot(`
-      Object {
-        "data": Array [
-          Object {
-            "key": "did:ethr:0xE712878f6E8d5d4F9e87E10DA604F9cB564C9a89#controller",
-            "location": "example.tradetrust.io",
-            "status": "VALID",
-          },
-          Object {
-            "reason": Object {
-              "code": 3,
-              "codeString": "INVALID_ISSUERS",
-              "message": "Issuer is not using DID-DNS identityProof type",
-            },
-            "status": "INVALID",
-          },
-        ],
-        "name": "OpenAttestationDnsDidIdentityProof",
-        "status": "INVALID",
-        "type": "ISSUER_IDENTITY",
-      }
-    `);
-
-    const invalidFragment = await openAttestationDnsDidIdentityProof.verify(
-      documentDnsDidMixedTokenRegistryInvalid,
-      options
-    );
-    expect(invalidFragment).toMatchInlineSnapshot(`
-      Object {
-        "data": Array [
-          Object {
-            "key": "did:ethr:0xE712878f6E8d5d4F9e87E10DA604F9cB564C9a89#controller",
-            "location": "example.com",
-            "status": "INVALID",
-          },
-          Object {
-            "reason": Object {
-              "code": 3,
-              "codeString": "INVALID_ISSUERS",
-              "message": "Issuer is not using DID-DNS identityProof type",
-            },
-            "status": "INVALID",
-          },
-        ],
-        "name": "OpenAttestationDnsDidIdentityProof",
-        "status": "INVALID",
-        "type": "ISSUER_IDENTITY",
-      }
-    `);
+          "name": "OpenAttestationDnsDidIdentityProof",
+          "status": "INVALID",
+          "type": "ISSUER_IDENTITY",
+        }
+      `);
+    });
   });
 });
--- src/verifiers/issuerIdentity/dnsDid/dnsDidProof.ts
+++ src/verifiers/issuerIdentity/dnsDid/dnsDidProof.ts
@@ -1,6 +1,11 @@
 import { v2, v3, WrappedDocument, getData, utils } from "@govtechsg/open-attestation";
 import { getDnsDidRecords } from "@govtechsg/dnsprove";
-import { VerificationFragmentType, Verifier } from "../../../types/core";
+import {
+  VerificationFragmentType,
+  Verifier,
+  VerificationFragment,
+  VerificationFragmentStatus,
+} from "../../../types/core";
 import { OpenAttestationDnsDidCode } from "../../../types/error";
 import { withCodedErrorHandler } from "../../../common/errorHandler";
 import { CodedError } from "../../../common/error";
@@ -23,9 +28,13 @@ const skip: VerifierType["skip"] = async () => {
 };
 
 const test: VerifierType["test"] = (document) => {
-  if (!utils.isSignedWrappedV2Document(document)) return false;
-  const data = getData(document);
-  if (data?.issuers.some((issuer) => issuer.identityProof?.type === "DNS-DID")) return true;
+  if (utils.isWrappedV2Document(document)) {
+    const data = getData(document);
+    return !!data?.issuers.some((issuer) => issuer.identityProof?.type === "DNS-DID");
+  }
+  if (utils.isWrappedV3Document(document)) {
+    return document.openAttestationMetadata.identityProof.type === v3.IdentityProofType.DNSDid;
+  }
   return false;
 };
 
@@ -35,7 +44,7 @@ interface IdentityProof {
   location?: string;
 }
 
-interface VerificationFragment {
+interface DnsVerificationFragment {
   status: "VALID" | "SKIPPED" | "INVALID";
   location?: string;
   key?: string;
@@ -44,12 +53,10 @@ interface VerificationFragment {
 const verifyIssuerDnsDid = async ({
   key,
   location,
-  type: identityType,
-}: IdentityProof): Promise<VerificationFragment> => {
-  if (identityType !== "DNS-DID")
-    return {
-      status: "SKIPPED",
-    };
+}: {
+  key?: string;
+  location?: string;
+}): Promise<DnsVerificationFragment> => {
   if (!location)
     throw new CodedError(
       "location is not present in identity proof",
@@ -70,33 +77,64 @@ const verifyIssuerDnsDid = async ({
   };
 };
 
+const verifyV2 = async (document: v2.WrappedDocument): Promise<VerificationFragment> => {
+  if (!utils.isSignedWrappedV2Document(document))
+    throw new CodedError(
+      "document is not signed",
+      OpenAttestationDnsDidCode.UNSIGNED,
+      OpenAttestationDnsDidCode[OpenAttestationDnsDidCode.UNSIGNED]
+    );
+  const documentData = getData(document);
+  const deferredVerificationStatus: Promise<DnsVerificationFragment>[] = documentData.issuers.map((issuer) => {
+    if (issuer.identityProof?.type === "DNS-DID")
+      return verifyIssuerDnsDid({ key: issuer.identityProof.key, location: issuer.identityProof.location });
+    return Promise.resolve({
+      status: "INVALID",
+      reason: {
+        message: "Issuer is not using DID-DNS identityProof type",
+        code: OpenAttestationDnsDidCode.INVALID_ISSUERS,
+        codeString: OpenAttestationDnsDidCode[OpenAttestationDnsDidCode.INVALID_ISSUERS],
+      },
+    });
+  });
+  const verificationStatus = await Promise.all(deferredVerificationStatus);
+  const overallStatus =
+    verificationStatus.some((status) => status.status === "VALID") &&
+    verificationStatus.every((status) => status.status === "VALID")
+      ? "VALID"
+      : "INVALID";
+  return {
+    name,
+    type,
+    data: verificationStatus,
+    status: overallStatus,
+  };
+};
+
+const verifyV3 = async (document: v3.WrappedDocument): Promise<VerificationFragment> => {
+  if (!utils.isSignedWrappedV3Document(document))
+    throw new CodedError(
+      "document is not signed",
+      OpenAttestationDnsDidCode.UNSIGNED,
+      OpenAttestationDnsDidCode[OpenAttestationDnsDidCode.UNSIGNED]
+    );
+  const location = document.openAttestationMetadata.identityProof.identifier;
+  const { key } = document.proof;
+  const verificationStatus = await verifyIssuerDnsDid({ key, location });
+
+  return {
+    name,
+    type,
+    data: verificationStatus,
+    status: verificationStatus.status,
+  };
+};
+
 const verify: VerifierType["verify"] = withCodedErrorHandler(
   async (document) => {
-    if (!utils.isSignedWrappedV2Document(document)) throw new Error("Only v2 is supported now");
-    const documentData = getData(document);
-    const deferredVerificationStatus: Promise<VerificationFragment>[] = documentData.issuers.map((issuer) => {
-      if (issuer.identityProof?.type === "DNS-DID") return verifyIssuerDnsDid(issuer.identityProof);
-      return Promise.resolve({
-        status: "INVALID",
-        reason: {
-          message: "Issuer is not using DID-DNS identityProof type",
-          code: OpenAttestationDnsDidCode.INVALID_ISSUERS,
-          codeString: OpenAttestationDnsDidCode[OpenAttestationDnsDidCode.INVALID_ISSUERS],
-        },
-      });
-    });
-    const verificationStatus = await Promise.all(deferredVerificationStatus);
-    const overallStatus =
-      verificationStatus.some((status) => status.status === "VALID") &&
-      verificationStatus.every((status) => status.status === "VALID")
-        ? "VALID"
-        : "INVALID";
-    return {
-      name,
-      type,
-      data: verificationStatus,
-      status: overallStatus,
-    };
+    if (utils.isWrappedV2Document(document)) return verifyV2(document);
+    if (utils.isWrappedV3Document(document)) return verifyV3(document);
+    throw new Error("");
   },
   {
     name,
