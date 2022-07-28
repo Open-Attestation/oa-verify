35cb4bf2209560d12700589d18de128172745e20
marlowl <marlowlieshout@hotmail.com>
2022-07-28 11:58:52 +0200

    fix: false positive warning in verificationBuilder

--- src/verifiers/verificationBuilder.ts
+++ src/verifiers/verificationBuilder.ts
@@ -17,19 +17,17 @@ import { warnProvider } from "../common/messages";
  * Before running each verifier, the manager will make sure the verifier can handle the specific document by calling its exposed test function.
  * The manager will return the consolidated list of {@link VerificationFragment}
  */
-let displayWarning = true;
 export const verificationBuilder = <T extends Verifier<any>>(
   verifiers: T[],
   builderOptions: VerificationBuilderOptions
 ) => (document: DocumentsToVerify, promisesCallback?: PromiseCallback): Promise<VerificationFragment[]> => {
   // if the user didn't configure an API key and didn't configure a provider or a resolver, then he will likely use a development key. We then warn him once, that he may need to configure things properly, especially for production
   if (
-    displayWarning &&
-    (!builderOptions.resolver || !builderOptions.provider) &&
+    !builderOptions.resolver &&
+    !builderOptions.provider &&
     !process.env.INFURA_API_KEY &&
     !process.env.PROVIDER_API_KEY
   ) {
-    displayWarning = false;
     console.warn(warnProvider);
   }
   const verifierOptions: VerifierOptions = {
