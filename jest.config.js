ef4a0f0ebdd3fd3e173cb55a94fd912cd99f2a5d
Kyle Huang Junyuan <kyle@hjunyuan.com>
2023-11-03 09:46:12 +0800

    fix: temporary workaround for CJS Axios

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
   testEnvironment: "node",
   watchPlugins: ["jest-watch-typeahead/filename", "jest-watch-typeahead/testname"],
   testTimeout: 30000,
+  moduleNameMapper: {
+    axios: "axios/dist/node/axios.cjs", // Temporary workaround: Force Jest to import the CommonJS Axios build
+  },
 };
