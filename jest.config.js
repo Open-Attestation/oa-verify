c6b15c62be149e02a6ca1ce0e8b49ae5ea2360ab
Seth <hullogh@gmail.com>
2024-04-05 02:03:04 +0800

    chore: jest config

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,5 @@ module.exports = {
   moduleNameMapper: {
     axios: "axios/dist/node/axios.cjs", // Temporary workaround: Force Jest to import the CommonJS Axios build
   },
+  transformIgnorePatterns: ["node_modules/(?!(@govtechsg/document-store-ethers-v5)/)"],
 };
